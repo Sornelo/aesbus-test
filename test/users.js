98d880f67328e6efec6204d73afd5bd821e7dbe6
Dan White <dwhite4114@gmail.com>
2020-10-22 13:14:59 -0400

    add tests

---
+++ test/users.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+describe('Users', function() {
+  describe('#getMany()', function() {
+    it('should return a list of users', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('#getSingle()', function() {
+    it('should return a user if user exists', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('should throw a ResourceNotFound error if users doesn\'t exist', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('should throw a BadRequest error if id isn\'t provided', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
